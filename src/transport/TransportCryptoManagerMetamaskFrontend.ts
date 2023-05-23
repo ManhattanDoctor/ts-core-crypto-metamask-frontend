849d2637f56e6ea7c3dc4c4148bd547594fb4162
Renat Gubaev <gubaev.r@n-t.io>
2023-05-23 16:53:01 +0300

    auto commit

--- src/transport/TransportCryptoManagerMetamaskFrontend.ts
+++ src/transport/TransportCryptoManagerMetamaskFrontend.ts
@@ -1,15 +1,16 @@
 import { ITransportCommand, ISignature, TransportCryptoManager } from '@ts-core/common';
+import { Metamask } from '../Metamask';
 import * as _ from 'lodash';
 
 export class TransportCryptoManagerMetamaskFrontend extends TransportCryptoManager {
-    
+
     // --------------------------------------------------------------------------
     //
     //  Static Methods
     //
     // --------------------------------------------------------------------------
 
-    public static ALGORITHM = 'Ed25519Metamask';
+    public static ALGORITHM = Metamask.ALGORITHM;
 
     // --------------------------------------------------------------------------
     //
@@ -28,12 +29,11 @@ export class TransportCryptoManagerMetamaskFrontend extends TransportCryptoManag
     // --------------------------------------------------------------------------
 
     public async sign<U>(command: ITransportCommand<U>, nonce: string, address: string): Promise<string> {
-        return this.wallet.request({ method: 'personal_sign', params: [this.toString(command, nonce), address] });
+        return Metamask.sign(this.toString(command, nonce), address, this.wallet);
     }
 
     public async verify<U>(command: ITransportCommand<U>, signature: ISignature): Promise<boolean> {
-        let address = await this.wallet.request({ method: 'personal_ecRecover', params: [this.toString(command, signature.nonce), signature.value] });
-        return address.toUpperCase() === signature.publicKey.toUpperCase();
+        return Metamask.verify(this.toString(command, signature.nonce), signature.value, signature.publicKey, this.wallet);
     }
 
     // --------------------------------------------------------------------------
